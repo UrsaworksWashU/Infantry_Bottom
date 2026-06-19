b7e5cf51ced87c8d7b0dab4df4ac20b08efc7d15
eaeaeaosn <eeeaaason070311@gmail.com>
2026-06-19 17:34:25 -0500

    tuned pitch, need to tune ff in vision

--- application/robot_def.h
+++ application/robot_def.h
@@ -31,7 +31,7 @@
 #define PITCH_HORIZON_ECD 7609      // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
 #define PITCH_MAX_ANGLE 25.0f       // 云台竖直方向最大角度,陀螺仪deg,向上为正,根据机械限位实测后修改
 #define PITCH_MIN_ANGLE -14.0f      // 云台竖直方向最小角度,陀螺仪deg,向下为负,根据机械限位实测后修改
-#define PITCH_GRAVITY_FF_COEF 1400.0f // pitch重力补偿前馈系数,电流单位,实测标定
+#define PITCH_GRAVITY_FF_COEF 3600.0f // pitch重力补偿前馈系数,电流单位,实测标定
 // 发射参数
 #define ONE_BULLET_DELTA_ANGLE 45.0f    // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
 #define REDUCTION_RATIO_LOADER 36.0f // 2006拨盘电机的减速比,英雄需要修改为3508的19.0f
@@ -84,7 +84,8 @@ typedef enum
 typedef enum
 {
     CHASSIS_ZERO_FORCE = 0,    // 电流零输入
-    CHASSIS_ROTATE,            // 小陀螺模式
+    CHASSIS_ROTATE_CLOCKWISE,            // 小陀螺模式
+    CHASSIS_ROTATE_COUNTERCLOCKWISE,
     CHASSIS_NO_FOLLOW,         // 不跟随，允许全向平移
     CHASSIS_FOLLOW_GIMBAL_YAW, // 跟随模式，底盘叠加角度环控制
 } chassis_mode_e;
